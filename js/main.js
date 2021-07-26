6342fd6e553a76adb8c86b62fc762e44694656e2
Víctor Izquierdo <vhmizq@gmail.com>
2021-07-26 14:45:02 -0500

    finished

---
+++ js/main.js
@@ -0,0 +1,41 @@
+//initial settings
+const images = [];
+const time = 3000;
+let index = 0;
+const prevBtn = document.querySelector(".prev-button");
+const nextBtn = document.querySelector(".next-button");
+
+//Image list
+images[0] = "/imgs/img1.jpg";
+images[1] = "/imgs/img2.jpg";
+images[2] = "/imgs/img3.jpg";
+images[3] = "/imgs/img4.jpg";
+
+//selecting img buttons function
+nextBtn.addEventListener("click", () =>{
+    document.slide.src = images[index];
+    if(index < images.length -1){
+        index++;
+    }
+})
+prevBtn.addEventListener("click", ()=>{
+    document.slide.src = images[index];
+    index--;
+    if(index < 0){
+        index = images.length - 1;
+    }
+})
+
+
+//change img time function
+/* function changeImage(){
+    document.slide.src = images[i];
+    if(i < images.length - 1){
+        i++
+    }else{
+        i = 0;
+    }
+    setTimeout("changeImage()", time);
+} */
+
+
